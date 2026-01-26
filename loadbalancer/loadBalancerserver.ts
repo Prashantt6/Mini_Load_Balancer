2ebfa1a73afe51ce996c91983287efc01435a33a
Prashantt6 <royprashant14@gmail.com>
2026-01-26 16:47:55 +0545

    Server id added in Hash ring

--- loadbalancer/loadBalancerserver.ts
+++ loadbalancer/loadBalancerserver.ts
@@ -7,7 +7,12 @@ const app = express()
 const router = express.Router()
 app.use(express.json())
 
-
+function addServer() {
+    addServerId("server-1");
+    addServerId("server-2");
+    addServerId("server-3");
+}
+addServer()
 app.use((req,res) =>{
     // res.send("This is the loadbalancer server")
     const key = req.path 
