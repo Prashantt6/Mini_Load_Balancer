5d0576a145facff95be86d74ebc7be3e87b8027e
Prashantt6 <royprashant14@gmail.com>
2026-01-26 17:21:42 +0545

    Loadbalancer working well

--- loadbalancer/loadBalancerserver.ts
+++ loadbalancer/loadBalancerserver.ts
@@ -1,10 +1,18 @@
+import httpProxy from 'http-proxy'
 import express from 'express'
 import { getServerId } from './lb-algorithm/consistentHashing'
 import { addServerId } from './lb-algorithm/consistentHashing'
 
 
 const app = express()
-const router = express.Router()
+const proxy = httpProxy.createProxyServer({})
+
+const serverMap: Record<string, string> ={
+    "server-1": "http://localhost:3001",
+    "server-2": "http://localhost:3002",
+    "server-3": "http://localhost:3003"
+}
+
 app.use(express.json())
 
 function addServer() {
@@ -17,8 +25,11 @@ app.use((req,res) =>{
     // res.send("This is the loadbalancer server")
     const key = req.path 
     const serverId = getServerId(key)
+    console.log("Routing to:", serverId, serverMap[serverId]);
+
+    proxy.web(req, res, {
+        target: serverMap[serverId]
+    })
 })
 
-app.listen(8000, () =>{
-    console.log("Server is running at port 8000")
-})
+app.listen(8000);
